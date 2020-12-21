f5ede8f184180e1db248ce88f96867bb19cf7f8e
ehmusman <ehmusman@gmail.com>
2020-12-21 10:21:34 +0500

    react concurrent mode is enabled

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ENable concurrent mode
+ReactDOM.unstable_createRoot(document.getElementById('root')).render(<App />);
 
